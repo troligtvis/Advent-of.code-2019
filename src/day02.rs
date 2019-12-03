5971cc1eed040f766da759bfd7b2bd786ffe6b4d
troligtvis <karljohan@codefork.se>
2019-12-03 22:24:40 +0100

    Add tests

--- src/day02.rs
+++ src/day02.rs
@@ -93,3 +93,19 @@ impl OpCode {
         }
     }
 }
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_opcode_calculation() {
+        let noun = 0;
+        let verb = 0;
+        assert_eq!(int_code(&mut vec![1,0,0,0,99], noun, verb), Some(2));
+        assert_eq!(int_code(&mut vec![2,3,0,3,99], noun, verb), Some(2));
+        assert_eq!(int_code(&mut vec![2,4,4,5,99,0], noun, verb), Some(2));
+        assert_eq!(int_code(&mut vec![1,1,1,4,99,5,6,0,99], noun, verb), Some(30));
+    }
+}
